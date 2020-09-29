5f384344c129fd49406ef80443dca92781cd683d
wangchong <wangchong@didachuxing.com>
2020-09-29 10:27:14 +0800

    farmhash_linux

--- farmhash.js
+++ farmhash.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const platform = os.platform();
 
-if (platform === 'linux') {
+if (platform === 'linux' && process.version.startsWith('v10')) {
   module.exports = require('./farmhash_linux/index');
 } else {
   module.exports = require('farmhash');
